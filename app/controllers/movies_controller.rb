ba2f3153070c2d519d493f3cac04ec83838ffc29
Somesh Yadav <syb777yadav@gmail.com>
2024-07-14 18:28:47 +0530

    [07] - Adds CRUD operations on movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,14 +12,32 @@ class MoviesController < ApplicationController
   end
 
   def create
+    if @movie.save
+      redirect_to @movie, notice: 'Movie was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: 'Movie was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @movie.destroy
+    redirect_to movies_url, notice: 'Movie was successfully destroyed.'
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :genre)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,9 @@ Movie.create!(title: 'Kalki 2898 AD', genre: 'Action/Sci-Fi/Thriller')
 Movie.create!(title: 'Deadpool & Wolverine', genre: 'Action/Adventure/Comedy')
 Movie.create!(title: 'Sarfira', genre: 'Biography/Drama')
 Movie.create!(title: 'Kill', genre: 'Action/Thriller')
+
+# Create admin user
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: :admin)
+
+# Create normal user
+User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
